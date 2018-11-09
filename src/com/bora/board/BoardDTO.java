4123b3aabe03a91578eafeb6f1c6c60e4211489a
test <test1>
2018-11-09 15:43:03 +0900

    fixed BoardDTO

--- src/com/bora/board/BoardDTO.java
+++ src/com/bora/board/BoardDTO.java
@@ -1,5 +1,51 @@
 package com.bora.board;
 
+import java.sql.Date;
+
 public class BoardDTO {
+	private int num;
+	private String title;
+	private String writer;
+	private String contents;
+	private Date reg_date;
+	private int hit;
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public Date getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+	
 
 }
