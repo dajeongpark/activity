c7f78f7acefb6677b88019a9617a6af63c6789d5
test <test1>
2018-11-09 18:59:05 +0900

    1109 commit
    act_cyj

--- src/com/bora/activity/ActivityService.java
+++ src/com/bora/activity/ActivityService.java
@@ -1,5 +1,35 @@
 package com.bora.activity;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bora.action.ActionForward;
+
 public class ActivityService {
+	
+	private ActionForward actionForward;
+	
+	public ActivityService() {
+		actionForward = new ActionForward();
+	}
+	
+	//selectList
+	public ActionForward selectList(HttpServletRequest request, HttpServletResponse response) {
+		ActionForward actionForward = new ActionForward();
+		
+		
+		return actionForward;
+	}
+	
+	//selectOne
+	public void selectOne() {
+		
+	}
+	
+	//insert
+	
+	//update
+	
+	//delete
 
 }
