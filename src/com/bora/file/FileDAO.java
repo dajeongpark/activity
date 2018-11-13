b5d2cfe764933e41a866d7d6fb57e2dc86679c97
test <test@>
2018-11-13 17:28:03 +0900

    fileDAO/DTO/Service
    commit

--- src/com/bora/file/FileDAO.java
+++ src/com/bora/file/FileDAO.java
@@ -1,5 +1,73 @@
 package com.bora.file;
 
-public class FileDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.bora.util.DBConnector;
+
+public class FileDAO {
+	public int delete(int fnum) throws Exception {
+		Connection con = DBConnector.getConnect();
+		String sql = "delete upload where fnum=?";
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setInt(1, fnum);
+		
+		int result = st.executeUpdate();
+		DBConnector.disConnect(st, con);
+		return result;
+	}
+	
+	public List<FileDTO> selectList(FileDTO fileDTO) throws Exception {
+		List<FileDTO> ar = new ArrayList<>();
+		Connection con = DBConnector.getConnect();
+		String sql = "select * from upload where num=? and kind=?";
+		
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setInt(1, fileDTO.getNum());
+		st.setString(2, fileDTO.getKind());
+		ResultSet rs = st.executeQuery();
+		
+		while(rs.next()) {
+			fileDTO = new FileDTO();
+			fileDTO.setFnum(rs.getInt("fnum"));
+			fileDTO.setFname(rs.getString("fname"));
+			fileDTO.setOname(rs.getString("oname"));
+			fileDTO.setNum(rs.getInt("num"));
+			fileDTO.setKind(rs.getString("kind"));
+			ar.add(fileDTO);
+		}
+		DBConnector.disConnect(rs, st, con);
+		return ar;
+	}
+	
+	public int insert(FileDTO fileDTO) throws Exception {
+		Connection con = DBConnector.getConnect();
+		String sql = "insert into upload values(file_seq.nextval,?,?,?,?)";
+		
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setString(1, fileDTO.getFname());
+		st.setString(2, fileDTO.getOname());
+		st.setInt(3, fileDTO.getNum());
+		st.setString(4, fileDTO.getKind());
+		
+		int result = st.executeUpdate();
+		DBConnector.disConnect(st, con);
+		
+		return result;
+	}
+	
+	public int deleteAll(int num) throws Exception {
+		Connection con = DBConnector.getConnect();
+		String sql = "delete upload where num=?";
+		
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setInt(1, num);
+		int result = st.executeUpdate();
+		DBConnector.disConnect(st, con);
+		
+		return result;
+	}
 }
--- src/com/bora/file/FileService.java
+++ src/com/bora/file/FileService.java
@@ -1,5 +1,47 @@
 package com.bora.file;
 
-public class FileService {
+import java.io.File;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bora.action.ActionForward;
+
+public class FileService {
+private FileDAO fileDAO;
+	
+	public FileService() {
+		fileDAO = new FileDAO();
+	}
+	
+	public ActionForward delete(HttpServletRequest request, HttpServletResponse response) {
+		ActionForward actionForward = new ActionForward();
+		
+		int fnum = 0;
+		try {
+			fnum = Integer.parseInt(request.getParameter("fnum"));
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		String fname = request.getParameter("fname");
+		try {
+			fnum = fileDAO.delete(fnum);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		if(fnum>0) {
+			String path = request.getServletContext().getRealPath("upload");
+			System.out.println(path);
+			File file = new File(path, fname);
+			file.delete();
+		}
+		request.setAttribute("message", fnum);
+		
+		actionForward.setCheck(true);
+		actionForward.setPath("../WEB-INF/view/common/resultAjax.jsp");
+		
+		return actionForward;
+	}
 }
