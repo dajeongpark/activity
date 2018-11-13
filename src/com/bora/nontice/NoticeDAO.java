6fa9b092b6961863cbc95e422e0ebd8cdfd4d04b
test <test@>
2018-11-13 10:19:56 +0900

    NoticeDAO_1
    commit

---
+++ src/com/bora/nontice/NoticeDAO.java
@@ -0,0 +1,69 @@
+package com.bora.nontice;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.List;
+
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
+
+import com.bora.board.BoardDAO;
+import com.bora.board.BoardDTO;
+import com.bora.page.RowNumber;
+import com.bora.page.Search;
+import com.bora.util.DBConnector;
+
+public class NoticeDAO implements BoardDAO {
+
+	@Override
+	public List<BoardDTO> selectList(RowNumber rowNumber) throws Exception {
+		Connection con = DBConnector.getConnect();
+		String sql="";
+		PreparedStatement st = con.prepareStatement(sql);
+		
+		
+		return null;
+	}
+
+	@Override
+	public BoardDTO selectOne(int num) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insert(BoardDTO boardDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(BoardDTO boardDTO) throws Exception {
+		Connection con = DBConnector.getConnect();
+		String sql = "update notifce set title=? contents=? where num=?";
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setString(1, boardDTO.getTitle());
+		st.setString(2, boardDTO.getContents());
+		st.setInt(3, boardDTO.getNum());
+		int result = st.executeUpdate();
+		DBConnector.disConnect(st, con);
+		return result;
+	}
+
+	@Override
+	public int delete(int num) throws Exception {
+		Connection con = DBConnector.getConnect();
+		String sql = "delete notice where num=?";
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setInt(1, num);
+		int result = st.executeUpdate();
+		DBConnector.disConnect(st, con);
+		return result;
+	}
+
+	@Override
+	public int getCount(Search search) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
