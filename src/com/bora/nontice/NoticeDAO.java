12996d3f9f68a257edd344c19daad73e81be3e85
test <test@>
2018-11-14 17:01:14 +0900

    Revert "NoticeDAO_selectOne&insert"
    This reverts commit 09de6aeb7d2ced2718c6f307b52c545d44250b9f.

--- src/com/bora/nontice/NoticeDAO.java
+++ src/com/bora/nontice/NoticeDAO.java
@@ -2,7 +2,6 @@ package com.bora.nontice;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.List;
 
 import com.bora.board.BoardDAO;
@@ -25,38 +24,14 @@ public class NoticeDAO implements BoardDAO {
 
 	@Override
 	public BoardDTO selectOne(int num) throws Exception {
-		Connection con = DBConnector.getConnect();
-		String sql = "select * from notice where num=?";
-		
-		PreparedStatement st = con.prepareStatement(sql);
-		st.setInt(1, num);
-		ResultSet rs = st.executeQuery();
-		NoticeDTO ndto = new NoticeDTO();
-		if(rs.next()) {
-			ndto.setNum(rs.getInt("num"));
-			ndto.setTitle(rs.getString("title"));
-			ndto.setContents(rs.getString("contents"));
-			ndto.setWriter(rs.getString("writer"));
-			ndto.setReg_date(rs.getDate("reg_date"));
-			ndto.setHit(rs.getInt("hit"));
-		}
-		
-		DBConnector.disConnect(rs, st, con);
-		return ndto;
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
 	public int insert(BoardDTO boardDTO) throws Exception {
-		Connection con = DBConnector.getConnect();
-		String sql = "insert into notice values(?,?,?,?,sysdata,0)";
-		PreparedStatement st = con.prepareStatement(sql);
-		st.setInt(1, boardDTO.getNum());
-		st.setString(2, boardDTO.getTitle());
-		st.setString(3, boardDTO.getContents());
-		st.setString(4, boardDTO.getWriter());
-		int result= st.executeUpdate();
-		DBConnector.disConnect(st, con);
-		return result;
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 	@Override
