d8ada0dec4ee920a4b1dd14e7b98ca8007f6afa0
test <test@>
2018-11-16 17:59:25 +0900

    헤
    commit

---
+++ src/com/bora/controller/NoticeController.java
@@ -0,0 +1,68 @@
+package com.bora.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bora.action.ActionForward;
+import com.bora.notice.NoticeService;
+
+/**
+ * Servlet implementation class NoticeController
+ */
+@WebServlet("/NoticeController")
+public class NoticeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NoticeController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String phone = request.getServletContext().getInitParameter("phone");
+		System.out.println(phone);
+		String command = request.getPathInfo();
+		ActionForward actionForward = null;
+		NoticeService noticeService = new NoticeService();
+		
+		if(command.equals("/noticeList.do")) {
+			actionForward = noticeService.selectList(request, response);
+		}else if(command.equals("/noticeSelectOne.do")) {
+			actionForward = noticeService.selectOne(request, response);
+		}else if(command.equals("/noticeWrite.do")) {
+			actionForward = noticeService.insert(request, response);
+		}else if(command.equals("/noticeUpdate.do")) {
+			actionForward = noticeService.update(request, response);
+		}else if(command.equals("/noticeDelete.do")) {
+			actionForward = noticeService.delete(request, response);
+		}
+		
+		if(actionForward.isCheck()) {
+			RequestDispatcher view = request.getRequestDispatcher(actionForward.getPath());
+			view.forward(request, response);
+		}else {
+			response.sendRedirect(actionForward.getPath());
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- src/com/bora/notice/NoticeService.java
+++ src/com/bora/notice/NoticeService.java
@@ -1,5 +1,64 @@
 package com.bora.notice;
 
-public class NoticeService {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bora.action.ActionForward;
+import com.bora.board.BoardReplyService;
+import com.bora.file.FileDAO;
+
+public class NoticeService implements BoardReplyService{
+
+	@Override
+	public ActionForward selectList(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ActionForward selectOne(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ActionForward insert(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ActionForward update(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ActionForward delete(HttpServletRequest request, HttpServletResponse response) {
+		ActionForward actionForward = new ActionForward();
+		try {
+			int num = Integer.parseInt(request.getParameter("num"));
+			FileDAO fileDAO = new FileDAO();
+			fileDAO.deleteAll(num);
+			num = noticeDAO.delete(num);
+			
+			if(num>0) {
+				request.setAttribute("message", "Delete Success");
+				request.setAttribute("path", "./noticeList.do");
+			}else {
+				//update fail
+				request.setAttribute("message", "Delete Fail");
+				request.setAttribute("path", "./noticeList.do");
+			}
+		} catch (Exception e) {
+			request.setAttribute("message", "Delete Fail");
+			request.setAttribute("path", "./noticeList.do");
+			e.printStackTrace();
+		}
+		actionForward.setCheck(true);
+		actionForward.setPath("../WEB-INF/view/common/result.jsp");
+		
+		return actionForward;
+	}
 
 }
